75f039b3f87a6ac825270df9f1792e4bfb4ae3fd
chotaliya12345 <abhishekchotaliya>
2024-02-27 09:27:16 +0530

    28_2_24_DS

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React With Abhishek
+          Learn React Directory Stuctuer
         </a>
       </header>
     </div>
