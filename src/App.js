be751317b762d439a134dd3356da08207e0dceeb
chotaliya12345 <abhishekchotaliya>
2024-02-28 08:19:32 +0530

    28_2_24_DS

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Directory Stuctuer
+          Learn React Directory Stuctuer (Home Work In React)
         </a>
       </header>
     </div>
